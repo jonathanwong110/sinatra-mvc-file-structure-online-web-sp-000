c2ef27b780842df33a996c9ecd6c28ed999f8644
Jonathan Wong <jonathan.wong110@gmail.com>
2019-07-14 23:21:03 +0000

    Automatically backed up by Learn

--- app/models/model.rb
+++ app/models/model.rb
@@ -1,8 +1,9 @@
 class Dog < Sinatra::Base
   
   get '/name' do
-    @name = Dog.new
-    Dog.new("")
+    @name = Dog.new("rudolph")
+    @name = Dog.new("mastiff")
+    @name = Dog.new(2)
   end
 
 end
